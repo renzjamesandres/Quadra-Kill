d967401e389796b1030db70a77a949b5af8eb6f5
renzjamesandres <renzjamesandres68@gmail.com>
2024-12-19 21:55:03 +0800

    Update Square.java

--- Square.java
+++ Square.java
@@ -1,3 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package runquad;
+
+/**
+ *
+ * @author renzj
+ */
 public class Square extends Rectangle {
     @Override
     public void showDescription() {
