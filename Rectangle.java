f3458c3f69f8dbcb9f0f29ec83877e0a776510c1
renzjamesandres <renzjamesandres68@gmail.com>
2024-12-19 21:40:25 +0800

    Create Rectangle.java

---
+++ Rectangle.java
@@ -0,0 +1,7 @@
+public class Rectangle extends Quadrilateral {
+    @Override
+    public void showDescription() {
+        super.showDescription();
+        System.out.println("- has 4 right angles");
+    }
+}
