e27111a3c088543727c4ca686c7076133e4d2f8a
renzjamesandres <renzjamesandres68@gmail.com>
2024-12-19 21:43:16 +0800

    Create Square.java

---
+++ Square.java
@@ -0,0 +1,7 @@
+public class Square extends Rectangle {
+    @Override
+    public void showDescription() {
+        super.showDescription(); 
+        System.out.println("- has 4 equal sides");
+    }
+}
