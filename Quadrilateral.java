30174c9919d20cfe3899cc10bd55136d5940663f
renzjamesandres <renzjamesandres68@gmail.com>
2024-12-19 21:49:40 +0800

    Update Quadrilateral.java

--- Quadrilateral.java
+++ Quadrilateral.java
@@ -1,3 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package runquad;
+
+/**
+ *
+ * @author renzj
+ */
 public class Quadrilateral {
     public void showDescription() {
         System.out.println("- is quadrilateral");
